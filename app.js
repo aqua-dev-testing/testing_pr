884a3750f21ab9c6b09a412c4d9348fa7f114e8c
Fatima <66124862+fatima99s@users.noreply.github.com>
2025-12-16 12:50:06 +0000

    Add getUser and runCommand functions in app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const { exec } = require("child_process");
+
+function getUser(req) {
+  const userId = req.query.id;
+  const query = "SELECT * FROM users WHERE id=" + userId;
+  console.log(query);
+}
+
+function runCommand(cmd) {
+  exec(cmd); 
+}
+
+module.exports = { getUser, runCommand };
